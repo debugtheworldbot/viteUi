3d38831f7ff609d039cf2cc3c9e681a9df25d410
tian <924412409@qq.com>
2020-09-12 09:29:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Test/>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
