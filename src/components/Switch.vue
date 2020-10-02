349baf5cef12d1e664f04bda40206eb1c9cfb215
tian <istiancz@gmail.com>
2020-10-02 19:21:55 +0800

    pull away code to show user

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,21 +1,89 @@
 <template>
-  <Switches v-model:value="boolean" />
+  <div>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+      <Switch1/>
+      </div>
+      <div class="demo-actions">
+        <Buttons>查看代码</Buttons>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <Switch2/>
+      </div>
+      <div class="demo-actions">
+        <Buttons>查看代码</Buttons>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Switches from "../lib/Switches.vue"
-import {ref} from 'vue'
+import Switches from '../lib/Switches.vue'
+import Buttons from '../lib/Buttons.vue'
+import Switch1 from './Switch1.demo.vue'
+import Switch2 from './Switch2.demo.vue'
+import {
+  ref
+} from 'vue'
 
 export default {
-  name: "Switch",
-  components: {Switches},
+  components: {
+    Switch1,
+    Switches,
+    Buttons,
+    Switch2
+  },
   setup() {
-    const boolean = ref(true)
-    return { boolean}
+    const bool = ref(false)
+    return {
+      bool
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
 
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+
+  &-component {
+    padding: 16px;
+  }
+
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <Switches v-model:value="bool" disabled/>
+
+</template>
+
+<script lang="ts">
+import Switches from '../lib/Switches.vue'
+import {ref} from "vue";
+
+export default {
+  name: "Switch1.demo.vue",
+  components: {
+    Switches,
+  },
+  setup() {
+    const bool = ref(false)
+    return {
+      bool
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
